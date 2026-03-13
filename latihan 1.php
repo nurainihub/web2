10e7b7cd1226db917b944e80cc3abc2a99e72c2c
nurainihub <1703nuraini@gmail.com>
2026-03-13 19:55:55 +0700

    Add files via upload

--- latihan 1.php
+++ latihan 1.php
@@ -1,11 +1,13 @@
-<?php
-$A = 123; // variabel global
-
-function Test() {
-    $A = "Test"; // variabel lokal
-    echo "Nilai A dalam fungsi = $A\n";
-}
-
-Test();
-echo "Nilai A luar fungsi = $A\n"; 
-?>
+<?php  
+$a = 1; 
+$b = 2; 
+$c = $a.$b; 
+//nilai $c sekarang 12, menggabungkan angka 1 dan 2 
+$d= $c + 1 ; 
+echo $d;  
+// nilai $d 13,  
+$e = "Number"; 
+$f = $e.$d; 
+echo $f; 
+//nilai $f menjadi Number13, a kombinasi $e dan $f  
+?> 
