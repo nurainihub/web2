9b0b8993e3dc68705edb88f1c2b07c69aabb01b3
Nur'aini <1703nuraini@gmail.com>
2026-04-21 15:20:30 +0700

    Add files via upload

---
+++ Pertemuan 7/latihan 9.php
@@ -0,0 +1,19 @@
+<HTML>
+<HEAD>
+<TITLE>Penggunaan In Array</TITLE>
+</HEAD>
+<BODY>
+<?php
+$program = array("HTML", "PHP", "CSS", "JavaScript");
+print_r($program);
+
+$cari = "HTML";
+
+if (in_array($cari, $program)) {
+    echo "<br>Program Basis Web $cari ada di dalam array";
+} else {
+    echo "<br>Program Basis Web $cari tidak ada di dalam array";
+}
+?>
+</BODY>
+</HTML>
